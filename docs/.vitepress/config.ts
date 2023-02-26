871047521104f433d31978e1233fae3d20589916
Tony Trinh <tony19@gmail.com>
2023-02-26 17:35:42 -0600

    docs: remove prematurely added pt locale

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -91,7 +91,6 @@ export default defineConfig({
     zh: { label: '简体中文', link: 'https://cn.vitejs.dev' },
     ja: { label: '日本語', link: 'https://ja.vitejs.dev' },
     es: { label: 'Español', link: 'https://es.vitejs.dev' },
-    pt: { label: 'Português', link: 'https://pt.vitejs.dev' },
   },
 
   vue: {
